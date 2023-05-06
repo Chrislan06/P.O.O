c21029a4e969c136622c7155b957c84f213872ec
LucasVianaSampaio <lv6612530@gmail.com>
2023-05-06 16:08:07 -0300

    olá mindo

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,13 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        $db = db_connect();
-        $teste = $db->query('SELECT * FROM teste')->getResultObject();
-
-        foreach($teste as $t){
-            echo $t->id . '<br>';
-            echo $t->nome . '<br>';
-            echo $t->email . '<br>';
-        }
+        echo 'main';
     }
 }
