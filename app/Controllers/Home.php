b11623abfcfe58f95d9bf71d411d5165ef677b69
Chrislan Machado <Chrislan06@gmail.com>
2023-05-13 15:25:51 -0300

    Alterando nome dos arquivos CSS e JS

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,14 +4,16 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function _remap($method)
-    {
-        if(!session()->has('login')){
-            return redirect()->to('/login');
-        }
+    // public function _remap($method)
+    // {
+    //     if(!session()->has('login')){
+    //         return redirect()->to('/login');
+    //     }
 
-        return  $this->$method;
-    }
+    //     return  $this->$method;
+    // }
+
+    
     public function index()
     {
         echo 'logado';
