0e8ad3e6af31e9b9e3da51f2baf07429d31e29f9
CHRISLAN MACHADO DA COSTA <chrislan.costa@sti.ufc.br>
2023-05-15 14:45:31 -0300

    Testando Migration de Usuario

--- app/Database/Migrations/2023-05-15-173742_Usuario.php
+++ app/Database/Migrations/2023-05-15-173742_Usuario.php
@@ -8,11 +8,32 @@ class Usuario extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'nome' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'senha' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('usuario_migration');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('usuario_migration');
     }
 }
