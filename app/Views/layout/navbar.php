8706bfe1492b2aac57766dfce20c69ae98365c1a
CHRISLAN MACHADO DA COSTA <chrislan.costa@sti.ufc.br>
2023-05-24 16:19:50 -0300

    Testando Layout

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,12 @@
+<navbar id="navbar">
+      <div class="navbar-container">
+        <img src="assets/images/hotel-solid (1).svg" alt="" />
+        <ul class="navbar-items">
+          <a href="" class="navbar-item">Perfil</a>
+          <a href="" class="navbar-item">Pesquisar Reservas</a>
+          <a href="" class="navbar-item">Check-in</a>
+          <a href="" class="navbar-item">Check-out</a>
+          <a href="" class="navbar-item">Logout</a>
+        </ul>
+      </div>
+    </navbar>
