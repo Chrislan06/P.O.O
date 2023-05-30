46f347d16b49a95f65afc287f85014c4864a2549
CHRISLAN MACHADO DA COSTA <chrislan.costa@sti.ufc.br>
2023-05-30 18:22:20 -0300

    Criando model do quarto

--- app/Models/QuartoModel.php
+++ app/Models/QuartoModel.php
@@ -13,7 +13,7 @@ class QuartoModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['tipo','tipo_cama','tamanho','descricao'];
 
     // Dates
     protected $useTimestamps = true;
