57ee172db3e651525e15a1b39ac52b14f8178da4
CHRISLAN MACHADO DA COSTA <chrislan.costa@sti.ufc.br>
2023-06-02 17:51:27 -0300

    Acrescentando migration para reservas

--- app/Database/Migrations/2023-05-31-163543_Reserva.php
+++ app/Database/Migrations/2023-05-31-163543_Reserva.php
@@ -3,16 +3,60 @@
 namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
 
 class Reserva extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'data_check_in' => [
+                'type' => 'TIMESTAMP',
+                'null' =>  true,
+            ],
+            'data_check_out' => [
+                'type' => 'TIMESTAMP',
+                'null' =>  true,
+            ],
+            'data_inicio' => [
+                'type' => 'TIMESTAMP',
+                'null' =>  false,
+            ],
+            'data_fim' => [
+                'type' => 'TIMESTAMP',
+                'null' =>  false,
+            ],
+            'id_cliente' => [
+                'type' => 'int',
+                'null' =>  true,
+            ],
+            'id_quarto' => [
+                'type' => 'int',
+                'null' =>  false,
+            ],
+            'criado_em' => [
+                'type' => 'TIMESTAMP',
+                'default' => new RawSql('CURRENT_TIMESTAMP')
+            ],
+            'atualizado_em' => [
+                'type' => 'TIMESTAMP',
+                'default' => new RawSql('CURRENT_TIMESTAMP')
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('id_cliente', 'clientes', 'id', 'RESTRICT', 'SET NULL', 'fk_reservas_clientes');
+        $this->forge->addForeignKey('id_quarto', 'quartos', 'id', 'RESTRICT', 'CASCADE', 'fk_reservas_quartos');
+        $this->forge->createTable('blog');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('reservas');
     }
 }
