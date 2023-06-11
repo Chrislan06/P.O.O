c7324b0663243b1b632f57b73755e8b86d84bf2a
Chrislan Machado <Chrislan06@gmail.com>
2023-06-11 19:37:00 -0300

    criado seed para fotos de quartos

---
+++ app/Database/Seeds/Fotos.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Fotos extends Seeder
+{
+    public function run()
+    {
+        $db = db_connect();
+        $fTables = $db->table('fotos');
+
+        $fotos = [
+            [
+                'link' => 'suíte/LondonClingo/suiteLC',
+                'id_quarto' => 1
+            ],
+
+            [
+                'link' => 'suíte/heavenDoor/suiteHD',
+                'id_quarto' => 2
+            ],
+
+            [
+                'link' => 'casal/FRoom/casalFRoom',
+                'id_quarto' => 3
+            ],
+
+            [
+                'link' => 'casal/LDeluxe/casalLD',
+                'id_quarto' => 4
+            ],
+
+            [
+                'link' => 'solteiro/LHeart/solteiroLH',
+                'id_quarto' => 5
+            ],
+
+            [
+                'link' => 'solteiro/BadC/solteiroBC',
+                'id_quarto' => 6
+            ],
+
+            [
+                'link' => 'familia/ULove/familiaUL',
+                'id_quarto' => 7
+            ],
+
+            [
+                'link' => 'familia/GrassHome/familiaGH',
+                'id_quarto' => 8
+            ],
+        ];
+    }
+}
