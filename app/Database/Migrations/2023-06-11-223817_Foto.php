aab633f26af3c763938d92c155286fa86f61f812
Chrislan Machado <Chrislan06@gmail.com>
2023-06-11 19:41:16 -0300

    adicionando model e migration para fotos

---
+++ app/Database/Migrations/2023-06-11-223817_Foto.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Foto extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'id_quarto' => [
+                'type' => 'int',
+                'unsigned' => true,
+                'null' =>  false,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('id_quarto', 'quartos', 'id', 'RESTRICT', 'CASCADE', 'fk_reservas_quartos');
+        $this->forge->createTable('fotos',true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('fotos',true);
+    }
+}
